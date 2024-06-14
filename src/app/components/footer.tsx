7246dc74a49350629a13bfc8ecfc1e220aac53a9
Dan Vitoriano <vitoriano08@gmail.com>
2024-06-14 18:39:04 -0300

    fix footer link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <Link href="/" className="flex items-center">
               <img
                 src="/images/logo-dan-vitoriano.jpeg"
                 className="h-8 me-3"
@@ -15,7 +15,7 @@ export default function Footer() {
               <span className="self-center text-2xl font-semibold whitespace-nowrap">
                 Dan Vitoriano
               </span>
-            </a>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
