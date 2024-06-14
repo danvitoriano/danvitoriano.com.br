c5d15a70ed90631c740955f25de55ec55be42bb0
Dan Vitoriano <vitoriano08@gmail.com>
2024-06-14 17:47:47 -0300

    add monday at page event

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -11,7 +11,7 @@ export default function List() {
           <dl className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt className="text-sm font-medium leading-6 text-gray-900">
-                Reply Xchange
+                Reply Xchange <br /> <small>Evento Interno </small>
               </dt>
               <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                 13 de junho de 2024
@@ -23,45 +23,80 @@ export default function List() {
                 >
                   Reply Xchange
                 </a>
-                <br />
-                <small>Evento interno</small>
               </dd>
             </div>
           </dl>
-          <dl className="divide-y divide-gray-100">
+          <dl id="monday-page" className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt className="text-sm font-medium leading-6 text-gray-900">
-                Monday @ Page (Pride Edition)
+                Monday @ Page (Pride Edition) <br /> <small>Evento Interno </small>
               </dt>
               <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                 17 de junho de 2024
                 <br />
                 <a
+                  href="https://materiais.danvitoriano.com.br/diversidadetech"
+                  target="_blank"
+                  className="text-indigo-600 hover:text-indigo-500 font-bold"
+                >
+                  Futuro Inclusivo: Diversidade no Mercado de Tecnologia
+                </a>
+                <br />
+                Os desafios e oportunidades para a comunidade LGBTQIA+ e a importância da representatividade, da inclusão e da construção de um ambiente de trabalho mais diversos. Através de dados, exemplos e estratégias, convido empresas e profissionais a se engajarem na promoção da diversidade como um impulsionador da inovação.
+                <br />
+                <small>
+                  Mais sobre a Michael Page: <a
                   href="https://www.michaelpage.co.uk/advice/career-advice/being-happy-work/monday-motivation-how-be-happier-work"
                   target="_blank"
                   className="text-indigo-600 hover:text-indigo-500 font-bold"
                 >
                   Monday motivation: how to be happier at work
                 </a>
-                <br />
-                <small>Evento interno</small>
+
+                </small>
               </dd>
             </div>
           </dl>
           <dl className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt className="text-sm font-medium leading-6 text-gray-900">
-                FIAP + Alura
+                FIAP + Alura <br /> <small>Evento Interno </small>
               </dt>
               <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                17 de junho de 2024
+                21 de junho de 2024
                 <br />
-                LGBTQIAPN+ e a importância da compreensão, respeito e inclusão{" "}
-                <br />
-                <small>Evento interno</small>
+                A importância do letramento LGBTQIAPN+ como uma ferramenta fundamental para promover a compreensão, o respeito e a inclusão em todos os espaços e contextos. <br />
+                <a
+                  href="https://www.fiap.com.br/imprensa/dois-tercos-noticias-em-sao-paulo-evento-orgulho-tech-conf-celebra-a-diversidade-e-inclusao-lgbtqiapn-na-tecnologia-fiap/"
+                  target="_blank"
+                  className="text-indigo-600 hover:text-indigo-500 font-bold"
+                >
+                  FIAP + Alura e Orgulho Tech
+                </a>
               </dd>
             </div>
           </dl>
+          <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+              Mentoria para Todes
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                13 de julho de 2024
+                <br />
+                Será um dia de sessões de mentorias individuais gratuitas com especialistas e referências no mercado para potencializar a carreira de pessoas da nossa comunidade.
+<br />
+                <a
+                  href="https://www.linkedin.com/feed/update/urn:li:activity:7183418510436384768/"
+                  target="_blank"
+                  className="text-indigo-600 hover:text-indigo-500 font-bold"
+                >
+                  LinkedIn de Rafa Mores
+                </a>
+              </dd>
+            </div>
+          </dl>
+
           <dl className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
               <dt className="text-sm font-medium leading-6 text-gray-900">
