e11c34d7451c7dba4fab97d6f247db5dff444f4e
Dan Vitoriano <vitoriano08@gmail.com>
2024-12-12 13:06:11 -0300

    add async script loader to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import SimpleFooter from "./components/footer";
 import Header from "./components/header";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/21db23b6-9557-4dd7-bcb5-62eea936f818-loader.js" />
       <body
         className={
           inter.className +
