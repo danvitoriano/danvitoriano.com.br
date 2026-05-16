14307976f7235df06f5aea17e752933822a9cbdc
Dan Vitoriano <vitoriano08@gmail.com>
2026-05-16 19:02:45 -0300

    fix(footer): update copyright year to 2026

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -115,7 +115,7 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2025{" "}
+            © 2026{" "}
             <a href="https://danvitoriano.com.br/" className="hover:underline">
               Fullcircle Serviços de Informática LTDA
             </a>
